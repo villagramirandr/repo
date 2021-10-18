30aae7e9eda1ca331337851ce35a04c734828459
Miranda <mirandadrvillagra@gmail.com>
2021-10-18 20:30:05 -0300

    estoy guardando los cambios

---
+++ live.js
@@ -0,0 +1,131 @@
+
+// var a = 10
+// const b = 35
+
+// const suma = a + b
+// const resta = a - b
+// const multiplicacion = a * b
+// const division = a / b // slash
+// const modulo = 3 % 2 // el resto de la división
+
+// a++ // le suma 1 al valor actual
+// a-- // le resta 1 al valor actual
+
+// a +=  2 // le suma 2 al valor actual
+// a -=  2 // le resta 2 al valor actual
+
+// const edad = 10
+
+// const esMayorDeEdad = false 
+
+// // console.log (a);
+
+
+// // ------
+
+// const n1 = 7
+// const n2 = 20
+
+// // if (n1 >= 10 || n2 > 100) { 
+// // if (n1 >= 10 ) { 
+// //     console.log ("es maoyr a diez")
+    
+// // }   else{
+// //     console.log ("es menor a diez")
+// // }
+
+
+// // ---
+// // if (n1 >= 10){
+
+// // }
+// // else if(n1 < 5){
+// //     console.log("es menos a cinco")
+// // } else {
+// //     console.log ("no cumple con ninuna de las condiciones")
+// // }
+
+// // if (condicion) {
+    
+// // } else if (condicion2){
+
+// // } else {
+    
+// // }
+
+// const diaSemana = 7
+
+// switch (diaSemana){
+//     case 1: console.log ("lunes")
+//     break
+//     case 2: console.log ("martes")
+//     break
+//     case 3: console.log ("miercoles")
+//     break
+//     case 4: console.log ("jueves")
+//     break
+//     case 5: console.log ("viernes")
+//     break
+//     default: 
+//     console.log("fin de semana");
+// }
+
+
+// usuario - edad + condición
+const edad = 22
+
+if (edad >= 18 ){
+    console.log("el usuario es mayor de edad")
+} else {
+    console.log ("el usuario es menor de edad")
+}
+
+// ---
+
+// suma + resulatado + si es mayor a A o a B
+
+const m = 30
+const v = 20
+
+suma = m + v 
+console.log ( "" + "la suma es:" + suma)
+
+if (suma > m) {
+    console.log("es mayor a M")
+} else (suma > v);{
+    console.log("es mayor a V")
+}
+
+
+// ----
+
+// Generar una constante con un número
+// 2. Mostrar en pantalla si el número es par o impar
+
+// const num = 18
+
+
+
+
+// D. Día de semana
+// ```
+// 1. Crear una constante con el día de la semana
+// 2. Mostrar en pantalla si es fin de semana o no
+// ```
+
+const diaS = 7
+
+switch (diaS){
+    case 1: console.log ("lunes")
+    break
+    case 2: console.log ("martes")
+    break
+    case 3: console.log ("miercoles")
+    break
+    case 4: console.log ("jueves")
+    break
+    case 5: console.log ("viernes")
+    break
+    default: 
+    console.log("hoy es fin de semana");
+}
